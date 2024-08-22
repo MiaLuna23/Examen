b81e343dc06edfa6c2421f8ce586ffb2b655bf64
MiaLuna23 <166016904+MiaLuna23@users.noreply.github.com>
2024-08-22 08:17:23 -0300

    Add files via upload

--- examen.js
+++ examen.js
@@ -1,20 +1,31 @@
 let Frutas = []
 let Kilos = []
 
-for (let index = 0; index < array.length; index++) {
+for (let index = 0; index < frutas; index++) {
     console.log = (prompt ("ingresa una furta"))
     
 }
-for (let index = 0; index < array.length; index++) {
+for (let index = 0; index < kilos; index++) {
     console.log(prompt("ingrsa los kilos"))
     
 }
 
-if (frutas==true) {
+if (frutas==tomate) {
     console.log ("Hay"+ Frutas)
 } else {
     console.log ("No hay" + Fruta )
 
 }
-let kilos [0]= 20
-let kilos [1]= 20
+kilos [0](kilos+20)
+kilos [1] (kilos+20)
+ console.log (kilos.lenght)
+ let tamaños= Kilos.lenght
+ let mayorKilos = Kilos[0] 
+ for (let index = 0; index <tamaño; index++) {
+
+     if (kilos< mayorKilos)
+     {
+         mayorKilos=kilos[indice]
+     }
+ }
+ console.log ("El que tiene mas kilos es" + mayorKilos)
