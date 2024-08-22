a8b828eb34442f33072522b8b83a645515667b51
MiaLuna23 <166016904+MiaLuna23@users.noreply.github.com>
2024-08-22 07:59:27 -0300

    Add files via upload

---
+++ examen.js
@@ -0,0 +1,20 @@
+let Frutas = []
+let Kilos = []
+
+for (let index = 0; index < array.length; index++) {
+    console.log = (prompt ("ingresa una furta"))
+    
+}
+for (let index = 0; index < array.length; index++) {
+    console.log(prompt("ingrsa los kilos"))
+    
+}
+
+if (frutas==true) {
+    console.log ("Hay"+ Frutas)
+} else {
+    console.log ("No hay" + Fruta )
+
+}
+let kilos [0]= 20
+let kilos [1]= 20
